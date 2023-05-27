3e3c6754bfdf6552bb564387ce92478f2f88756a
AI-Amponsah <yawamp27@gmail.com>
2023-05-27 22:58:28 +0000

    apI

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+Python script that interacts with a REST API.
+"""
 import requests
 from sys import argv
 
-"""Python script that, using this REST API"""
+
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = int(argv[1])
@@ -12,8 +15,9 @@ if __name__ == "__main__":
     tasks = requests.get(task_endp).json()
     user_tasks = [task for task in tasks if task.get("userId") == employee_id]
     tasks_completed = [task for task in user_tasks if task.get("completed")]
-    print("Employee {} is done with tasks({}/{}):".format(name,
+
+    print("Employee {} is done with tasks ({}/{}):".format(name,
           len(tasks_completed), len(user_tasks)))
 
     for task in tasks_completed:
-        print("\t {}".format(task.get("title")))
+        print("\t{}".format(task.get("title")))
